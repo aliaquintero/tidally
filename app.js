6f35e2de760e9a86238f34bb084f5121437e3501
aliaquintero <aliaquintero@protonmail.com>
2023-03-25 21:38:11 -0400

    init test

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// const APIKey = "5796d926-4268-11ed-9c50-0242ac130002-5796d994-4268-11ed-9c50-0242ac130002";
-// const inputVal = input.value;
-// const url = `https://api.stormglass.io/v2/weather/point`;
-const submitSearch = document.querySelector('#submit-search');
-const weatherResults = document.querySelector('.weather-results');
+const userInput = document.querySelector("#search-input");
 
-submitSearch.addEventListener('submit', (e) => { 
-    e.preventDefault();
-    weatherResults.classList.add('visible');
-})
+
