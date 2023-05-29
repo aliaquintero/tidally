d766ca070e2fa245c428f97e5ee6afc4041c8f62
Alia Quintero <aliaquintero@gmail.com>
2023-05-28 23:58:32 -0400

    added styles, changed current location to window onload event instead of button event, update dom elements with data

--- app.js
+++ app.js
@@ -2,8 +2,19 @@
 
 const key = "4e1c8a419e1b49e4b92171950232605";
 const userQuery = document.querySelector("#search");
-const buttons = document.querySelectorAll("button");
-const currentWeatherResult = document.querySelector("#result");
+const form = document.querySelector("#search-form");
+const currentWeatherLocation = document.querySelector(
+  "#current-weather__location"
+);
+const currentWeatherTemp = document.querySelector(
+  "#current-weather__temperature"
+);
+const currentWeatherCondition = document.querySelector(
+  "#current-weather__conditions"
+);
+const currentTide = document.querySelector("#current-tide");
+const prevTide = document.querySelector("#prev-tide");
+const nextTide = document.querySelector("#next-tide");
 
 async function getWeather() {
   const date = new Date();
@@ -20,16 +31,24 @@ async function getWeather() {
   const currentWeather =
     marineData.forecast.forecastday[0].hour[date.getHours()];
 
-  currentWeatherResult.innerText = `${marineData.location.name} is currently ${currentWeather.condition.text} with a temperature of ${currentWeather.temp_f} F.`;
+  // displays data in dom
+  currentWeatherLocation.innerText = `${marineData.location.name}`;
+  currentWeatherTemp.innerText = `${currentWeather.temp_f} F`;
+  currentWeatherCondition.innerText = `${currentWeather.condition.text}`;
+  // currentTide.innerText = `The tide is: [rising/falling]`;
+  // prevTide.innerText = `Previous tide: [high/low] `;
+  // nextTide.innerText = `Next tide: [high/low]`;
 }
+// current location weather
+window.addEventListener("load", (e) => {
+  userQuery.value = "auto:ip";
+  getWeather();
+  userQuery.value = "";
+});
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (button.classList.contains("geolocation")) {
-      userQuery.value = "auto:ip";
-    }
-    getWeather();
-    userQuery.value = "";
-  });
+// search location weather
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeather();
+  userQuery.value = "";
 });
