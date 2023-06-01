c5937aa0f940d9c96b0be8d49d581c80ad463bbe
Alia Quintero <aliaquintero@gmail.com>
2023-05-31 20:55:31 -0400

    wrote renderTides conditionals (prevTide, nextTide, rising/falling)

--- app.js
+++ app.js
@@ -28,12 +28,46 @@ function renderWeather(data) {
 
 function renderTides(data) {
   const currentTime = new Date();
+  const tides = [];
   const tideData = data.forecast.forecastday[0].day.tides[0].tide;
   tideData.forEach((tide) => {
     const tide_type = tide.tide_type;
     const tide_time = tide.tide_time;
     const tideTime = new Date(tide_time);
+    tides.push({ time: tideTime, type: tide_type });
   });
+
+  let prevTide = null;
+  let nextTide = null;
+
+  // find previous and next tides
+  for (let i = 0; i < tides.length; i++) {
+    if (tides[i].time < currentTime) {
+      prevTide = tides[i];
+    } else {
+      nextTide = tides[i];
+      break;
+    }
+  }
+  if (prevTide && nextTide) {
+    if (prevTide.type === "HIGH") {
+      console.log("The tide is falling");
+    } else if (prevTide.type === "LOW") {
+      console.log("The tide is rising");
+    }
+
+    console.log("Previous Tide:", prevTide.type);
+    console.log("Next Tide:", nextTide.type);
+  } else if (!prevTide) {
+    console.log("No data for previous tide");
+    console.log("Next Tide:", nextTide.type);
+  } else if (!nextTide) {
+    console.log("Previous Tide:", prevTide.type);
+
+    console.log("No data for next tide");
+  } else {
+    console.log("Not sufficient data. Sorry!");
+  }
 }
 
 // local weather on page load
